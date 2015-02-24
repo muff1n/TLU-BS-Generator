c4542b895b70b347e1fe7cf6c026600b8ec50a4d
Roger <roger@muff1n.eu>
2015-02-24 17:52:27 +0200

    First layout
    Praise jebus x2

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 	
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/bootstrap.css" media="screen">
+    	<link rel="stylesheet" href="css/bootswatch.min.css">
+	</head>
+	<body>
+		<div class="container" style="text-align: center;">
+		<h1>Bullshit generator</h1><br>
+		<input type="text" class="form-control" id="bullshitbox" style="font-size: 2em; height: 100px;"><br>
+		<button type="reset" class="btn btn-default">Generate bullshit</button>
+		</div>
+	</body>
+</html>
